43d987f563ffede6a6073a5668ceed7f6c1df0a5
Mayuresh <mayuresh.talekar87@gmail.com>
2025-05-14 07:01:46 +0530

    Code change A101Hello

--- src/newpackage/A101Hello.java
+++ src/newpackage/A101Hello.java
@@ -10,7 +10,7 @@ public class A101Hello {
 	// Java code can be executed without main method using static block.
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Worlds");
 	}
 }
 
